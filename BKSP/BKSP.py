d859dbdca1296083dfd7d7ca52e28e08149e1d0a
Pablo Penas <harbar2705@gmail.com>
2021-06-30 12:51:10 -0300

    Interface track model

--- BKSP/BKSP.py
+++ BKSP/BKSP.py
@@ -10,38 +10,76 @@ class Parameters:
         self.maxConcentration = maxConcentration
 
 
-def OleinikCriteria(left_concentration,right_concentration,data):
-    middlePoint = (left_concentration + right_concentration) / 2
-    f_left = data.interpolate(left_concentration)
-    f_right = data.interpolate(right_concentration)
-    f_middle = data.interpolate(middlePoint)
-    boundary = (f_right - f_left) / (right_concentration - left_concentration)
-    lower_boundary = (f_middle - f_left) / (right_concentration - left_concentration)
-    upper_boundary = (f_middle - f_right) / (left_concentration - right_concentration)
-    return lower_boundary <= boundary and upper_boundary >= boundary
+class InterfaceTrack:
+    def __init__(self,left_concentration,right_concentration,data):
+        self.left_concentration = left_concentration
+        self.right_concentration = right_concentration
+        self.data = data
 
-def InterfaceTrack(left_concentration,right_concentration,data):
-    if OleinikCriteria(left_concentration,right_concentration,data):
-        
+    
 
+    def OleinikCriteria(self):
+        self.middlePoint = (self.left_concentration + self.right_concentration) / 2
+        self.f_left = self.data.interpolate(self.left_concentration)
+        self.f_right = data.interpolate(self.right_concentration)
+        self.f_middle = data.interpolate(self.middlePoint)
+        boundary = (self.f_right - self.f_left) / (self.right_concentration - self.left_concentration)
+        lower_boundary = (self.f_middle - self.f_left) / (self.right_concentration - self.left_concentration)
+        upper_boundary = (self.f_middle - self.f_right) / (self.left_concentration - self.right_concentration)
 
-parameters = Parameters(0.1,0.3,5)
+        return lower_boundary <= boundary and upper_boundary >= boundary
+
+    def DescreaseRightLimit(self):
+        self.right_concentration -= 0.01
+
+    def DecreaseLeftLimit(self):
+        self.left_concentration -= 0.01
+
+    def IncreaseLeftLimit(self):
+        self.left_concentration += 0.01
+
+    def SetLeftLimit(self,concentration):
+        self.left_concentration = concentration
+
+    def SetRightLimit(self,concentration):
+        self.right_concentration = concentration
+
 dataSheet = pd.read_csv("BKSP/Dataset.csv")
 data = DataEntry.DataModel(dataSheet)
-# data.plot()
-if OleinikCriteria(0,parameters.initialConcentration,data):
-    f_0 = data.interpolate(0)
-    f_phi = data.interpolate(parameters.initialConcentration)
-    plt.plot([0,parameters.initialConcentration],[f_0, f_phi])
+parameters = Parameters(0.1,0.3,5)  
 
-if OleinikCriteria(parameters.initialConcentration,parameters.maxConcentration,data):
-    f_phi = data.interpolate(parameters.initialConcentration)
-    f_phimax = data.interpolate(parameters.maxConcentration)
-    plt.plot([parameters.initialConcentration,parameters.maxConcentration],[f_phi,f_phimax])
+upperInterface = InterfaceTrack(0,parameters.initialConcentration,data)
+lowerInterface = InterfaceTrack(parameters.initialConcentration,parameters.maxConcentration,data)
+
+if upperInterface.OleinikCriteria():
+    #Caso 1
+    plt.plot([0,parameters.initialConcentration],[upperInterface.f_left, upperInterface.f_right])
+
+if lowerInterface.OleinikCriteria():
+    #Caso 1
+    plt.plot([parameters.initialConcentration,parameters.maxConcentration],[lowerInterface.f_left, lowerInterface.f_right])
 else:
+    #Caso 2 e 3
+    hasFoundTan = False
+    while(not hasFoundTan):
+        if lowerInterface.right_concentration >= lowerInterface.left_concentration + 0.01:
+            lowerInterface.IncreaseLeftLimit()
+        else: 
+            hasFoundTan = True
+        if lowerInterface.OleinikCriteria():
+            hasFoundTan = True
+            plt.plot([lowerInterface.left_concentration,lowerInterface.right_concentration],[lowerInterface.f_left, lowerInterface.f_right])
 
+    #Caso 2
+    lowerInterface.SetLeftLimit(parameters.initialConcentration)
+    hasFoundTan = False
+    while(not hasFoundTan):
+        lowerInterface.DescreaseRightLimit()
+        if lowerInterface.OleinikCriteria():
+            hasFoundTan = True
+            plt.plot([lowerInterface.left_concentration,lowerInterface.right_concentration],[lowerInterface.f_left, lowerInterface.f_right])
 
-
+print(lowerInterface.left_concentration)
 plt.plot(data.concentrationData,data.fluxData)
 
 plt.show()
